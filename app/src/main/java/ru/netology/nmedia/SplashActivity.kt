fcb73ecb96c9bfc7d391907d6e999ded1db3bcf5
YoDa <netski@internet.ru>
2025-05-16 07:00:53 +0300

    Add icon

---
+++ app/src/main/java/ru/netology/nmedia/SplashActivity.kt
@@ -0,0 +1,23 @@
+package ru.netology.nmedia
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    private val splash: Long = 2000
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, splash)
+    }
+}
